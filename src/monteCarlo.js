a482fb5bde704f77fddeed6fb07ff7fa26401582
Arien Kock <arien.kock@gmail.com>
2026-03-14 11:29:18 +0100

    Delete task impl

--- src/monteCarlo.js
+++
@@ -1,7 +0,0 @@
-export function run(iterations, model, random = Math.random) {
-  const results = [];
-  for (let i = 0; i < iterations; i++) {
-    results.push(model.run(random));
-  }
-  return results;
-}
